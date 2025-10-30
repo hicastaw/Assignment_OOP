f064dcace47d5b355bef67445066ab16721b5c38
Nguyen Thang <nguyenthang7215@gmail.com>
2025-10-30 22:09:14 +0700

    feat: cập nhật phương thức và các tính chất

--- demo/src/main/java/com/ChessPTIT/model/King.java
+++ demo/src/main/java/com/ChessPTIT/model/King.java
@@ -1,68 +1,10 @@
 package com.ChessPTIT.model;
 
-import com.ChessPTIT.service.GameService; // <-- THÊM IMPORT NÀY
-import java.util.ArrayList;
-import java.util.List;
-
 public class King extends Piece {
-
     private boolean hasMoved = false;
 
     public King(PieceColor color) {
         super(color);
-    }
-
-    @Override
-    // THAY ĐỔI: Thêm GameService vào chữ ký phương thức
-    public List<Position> getValidMoves(Board board, Position currentPosition, GameService gameService) {
-        List<Position> moves = new ArrayList<>();
-        int currentRow = currentPosition.row();
-        int currentCol = currentPosition.col();
-
-        // 1. Tám nước đi một ô cơ bản
-        int[][] all_directions = {
-                { -1, -1 }, { -1, 0 }, { -1, 1 },
-                { 0, -1 }, { 0, 1 },
-                { 1, -1 }, { 1, 0 }, { 1, 1 }
-        };
-        for (int[] dir : all_directions) {
-            Position nextPos = new Position(currentRow + dir[0], currentCol + dir[1]);
-            if (isValid(nextPos)) {
-                Piece pieceAtNextPos = board.getPieceAt(nextPos);
-                if (pieceAtNextPos == null || pieceAtNextPos.getColor() != this.getColor()) {
-                    moves.add(nextPos);
-                }
-            }
-        }
-
-        // 2. LOGIC TẠO NƯỚC ĐI NHẬP THÀNH
-        if (!hasMoved()) { // Điều kiện 1: Vua chưa di chuyển
-            // Nhập thành cánh Vua (bên phải)
-            Piece kingSideRook = board.getPieceAt(new Position(currentRow, 7));
-            if (kingSideRook instanceof Rook && !((Rook) kingSideRook).hasMoved()) {
-                // Điều kiện 2 & 3: Xe chưa di chuyển và các ô ở giữa trống
-                if (board.getPieceAt(new Position(currentRow, 5)) == null &&
-                        board.getPieceAt(new Position(currentRow, 6)) == null) {
-                    moves.add(new Position(currentRow, 6)); // Thêm nước đi 2 ô của Vua
-                }
-            }
-
-            // Nhập thành cánh Hậu (bên trái)
-            Piece queenSideRook = board.getPieceAt(new Position(currentRow, 0));
-            if (queenSideRook instanceof Rook && !((Rook) queenSideRook).hasMoved()) {
-                if (board.getPieceAt(new Position(currentRow, 1)) == null &&
-                        board.getPieceAt(new Position(currentRow, 2)) == null &&
-                        board.getPieceAt(new Position(currentRow, 3)) == null) {
-                    moves.add(new Position(currentRow, 2));
-                }
-            }
-        }
-
-        return moves;
-    }
-
-    private boolean isValid(Position pos) {
-        return pos.row() >= 0 && pos.row() < 8 && pos.col() >= 0 && pos.col() < 8;
     }
 
     public boolean hasMoved() {
--- demo/src/main/java/com/ChessPTIT/model/Queen.java
+++ demo/src/main/java/com/ChessPTIT/model/Queen.java
@@ -1,55 +1,7 @@
 package com.ChessPTIT.model;
 
-import com.ChessPTIT.service.GameService; // <-- THÊM IMPORT NÀY
-import java.util.ArrayList;
-import java.util.List;
-
 public class Queen extends Piece {
-
     public Queen(PieceColor color) {
         super(color);
-    }
-
-    @Override
-    // THAY ĐỔI: Thêm GameService vào chữ ký phương thức
-    public List<Position> getValidMoves(Board board, Position currentPosition, GameService gameService) {
-        List<Position> moves = new ArrayList<>();
-        int currentRow = currentPosition.row();
-        int currentCol = currentPosition.col();
-
-        // Định nghĩa 8 hướng đi của Hậu (4 hướng của Xe + 4 hướng của Tượng)
-        int[][] directions = {
-                { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, // Hướng của Xe
-                { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } // Hướng của Tượng
-        };
-
-        // Đoạn code logic dưới đây giống hệt logic của quân Xe
-        for (int[] dir : directions) {
-            int nextRow = currentRow + dir[0];
-            int nextCol = currentCol + dir[1];
-
-            while (isValid(new Position(nextRow, nextCol))) {
-                Position nextPos = new Position(nextRow, nextCol);
-                Piece pieceAtNextPos = board.getPieceAt(nextPos);
-
-                if (pieceAtNextPos == null) {
-                    moves.add(nextPos);
-                } else {
-                    if (pieceAtNextPos.getColor() != this.getColor()) {
-                        moves.add(nextPos);
-                    }
-                    break;
-                }
-
-                nextRow += dir[0];
-                nextCol += dir[1];
-            }
-        }
-
-        return moves;
-    }
-
-    private boolean isValid(Position pos) {
-        return pos.row() >= 0 && pos.row() < 8 && pos.col() >= 0 && pos.col() < 8;
     }
 }
